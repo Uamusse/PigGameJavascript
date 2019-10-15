3f1446c0320ca230d1c1ef8e7caf8ba701b48903
Edson Uamusse <edson50uamusse@gmail.com>
2019-10-16 01:22:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         //Verificar se o jogador ganhou o jogo
-        if(scores[activePlayer]>=10){
+        if(scores[activePlayer]>=100){
             document.querySelector('#name-' + activePlayer).textContent = 'Winner';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
